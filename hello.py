41d51ebda9c2ee3f503b6872f80a4a38902480fa
TKTHdev <tkt0430@keio.jp>
2024-09-27 14:16:06 -0400

    Done activity 1.4

--- hello.py
+++ hello.py
@@ -1,15 +1,41 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
-from datetime import datetime
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hard to guess string'
+
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
-@app.route('/')
+class NameForm(FlaskForm):
+    name = StringField('What is your name?', validators=[DataRequired()])
+    email = StringField('What is your UofT Email?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html', name='Harunari', current_time=datetime.utcnow())
+    form = NameForm()
+    if form.validate_on_submit():
+        old_name = session.get('name')
+        old_email = session.get('email')
+        if old_name is not None and old_name != form.name.data:
+            flash('Looks like you have changed your name!')
+        if old_email is not None and old_email != form.email.data:
+            flash('Looks like you have changed your email!')
+        session['name'] = form.name.data
+        session['email'] = form.email.data
+        if 'utoronto' not in form.email.data:
+            session['email'] = None
+            flash('Please use a valid UofT email address.')
+        else:
+            flash(f"Your UofT email is {session['email']}.")
+        return redirect(url_for('index'))
+    return render_template('index.html', form=form, name=session.get('name'),
+                           email=session.get('email'))
 
 if __name__ == '__main__':
     app.run(debug=True)
